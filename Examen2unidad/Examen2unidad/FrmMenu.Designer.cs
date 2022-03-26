7ffb95ec2b4a7537205566bd85be8c5fbcabea48
ReneAMP <reneamp16@gmail.com>
2022-03-26 01:08:12 -0600

    Examen 2da Unidad
    Examen 2da Unidad

--- Examen2unidad/Examen2unidad/FrmMenu.Designer.cs
+++ Examen2unidad/Examen2unidad/FrmMenu.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.ribbonControlAdv1 = new Syncfusion.Windows.Forms.Tools.RibbonControlAdv();
-            this.toolStripTabItem1 = new Syncfusion.Windows.Forms.Tools.ToolStripTabItem();
             this.toolStripTabItem2 = new Syncfusion.Windows.Forms.Tools.ToolStripTabItem();
             this.toolStripEx1 = new Syncfusion.Windows.Forms.Tools.ToolStripEx();
             this.toolStripEx2 = new Syncfusion.Windows.Forms.Tools.ToolStripEx();
@@ -40,7 +39,6 @@
             this.NuevaFacturatoolStripButton1 = new System.Windows.Forms.ToolStripButton();
             ((System.ComponentModel.ISupportInitialize)(this.ribbonControlAdv1)).BeginInit();
             this.ribbonControlAdv1.SuspendLayout();
-            this.toolStripTabItem1.Panel.SuspendLayout();
             this.toolStripTabItem2.Panel.SuspendLayout();
             this.toolStripEx1.SuspendLayout();
             this.toolStripEx2.SuspendLayout();
@@ -53,7 +51,6 @@
             this.ribbonControlAdv1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ribbonControlAdv1.Dock = Syncfusion.Windows.Forms.Tools.DockStyleEx.Top;
             this.ribbonControlAdv1.Font = new System.Drawing.Font("Segoe UI", 8.25F);
-            this.ribbonControlAdv1.Header.AddMainItem(toolStripTabItem1);
             this.ribbonControlAdv1.Header.AddMainItem(toolStripTabItem2);
             this.ribbonControlAdv1.Header.AddMainItem(toolStripTabItem3);
             this.ribbonControlAdv1.Location = new System.Drawing.Point(0, 0);
@@ -72,7 +69,7 @@
             this.ribbonControlAdv1.QuickPanelImageLayout = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.ribbonControlAdv1.RibbonHeaderImage = Syncfusion.Windows.Forms.Tools.RibbonHeaderImage.None;
             this.ribbonControlAdv1.RibbonStyle = Syncfusion.Windows.Forms.Tools.RibbonStyle.Office2016;
-            this.ribbonControlAdv1.SelectedTab = this.toolStripTabItem2;
+            this.ribbonControlAdv1.SelectedTab = this.toolStripTabItem3;
             this.ribbonControlAdv1.ShowRibbonDisplayOptionButton = true;
             this.ribbonControlAdv1.Size = new System.Drawing.Size(768, 124);
             this.ribbonControlAdv1.SystemText.QuickAccessDialogDropDownName = "Start menu";
@@ -81,22 +78,6 @@
             this.ribbonControlAdv1.Text = "ribbonControlAdv1";
             this.ribbonControlAdv1.ThemeName = "Office2016";
             this.ribbonControlAdv1.ThemeStyle.MoreCommandsStyle.PropertyGridViewBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(171)))), ((int)(((byte)(171)))), ((int)(((byte)(171)))));
-            // 
-            // toolStripTabItem1
-            // 
-            this.toolStripTabItem1.Name = "toolStripTabItem1";
-            // 
-            // ribbonControlAdv1.ribbonPanel1
-            // 
-            this.toolStripTabItem1.Panel.Controls.Add(this.toolStripEx2);
-            this.toolStripTabItem1.Panel.Name = "ribbonPanel1";
-            this.toolStripTabItem1.Panel.ScrollPosition = 0;
-            this.toolStripTabItem1.Panel.TabIndex = 2;
-            this.toolStripTabItem1.Panel.Text = "Usuarios";
-            this.toolStripTabItem1.Position = 0;
-            this.toolStripTabItem1.Size = new System.Drawing.Size(68, 30);
-            this.toolStripTabItem1.Tag = "1";
-            this.toolStripTabItem1.Text = "Usuarios";
             // 
             // toolStripTabItem2
             // 
@@ -109,7 +90,7 @@
             this.toolStripTabItem2.Panel.ScrollPosition = 0;
             this.toolStripTabItem2.Panel.TabIndex = 3;
             this.toolStripTabItem2.Panel.Text = "Productos";
-            this.toolStripTabItem2.Position = 1;
+            this.toolStripTabItem2.Position = 0;
             this.toolStripTabItem2.Size = new System.Drawing.Size(75, 30);
             this.toolStripTabItem2.Tag = "2";
             this.toolStripTabItem2.Text = "Productos";
@@ -162,7 +143,7 @@
             this.toolStripTabItem3.Panel.ScrollPosition = 0;
             this.toolStripTabItem3.Panel.TabIndex = 4;
             this.toolStripTabItem3.Panel.Text = "Pedidos";
-            this.toolStripTabItem3.Position = 2;
+            this.toolStripTabItem3.Position = 1;
             this.toolStripTabItem3.Size = new System.Drawing.Size(64, 30);
             this.toolStripTabItem3.Tag = "3";
             this.toolStripTabItem3.Text = "Pedidos";
@@ -232,7 +213,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.ribbonControlAdv1)).EndInit();
             this.ribbonControlAdv1.ResumeLayout(false);
             this.ribbonControlAdv1.PerformLayout();
-            this.toolStripTabItem1.Panel.ResumeLayout(false);
             this.toolStripTabItem2.Panel.ResumeLayout(false);
             this.toolStripEx1.ResumeLayout(false);
             this.toolStripEx1.PerformLayout();
@@ -248,7 +228,6 @@
         #endregion
 
         private Syncfusion.Windows.Forms.Tools.RibbonControlAdv ribbonControlAdv1;
-        private Syncfusion.Windows.Forms.Tools.ToolStripTabItem toolStripTabItem1;
         private Syncfusion.Windows.Forms.Tools.ToolStripTabItem toolStripTabItem2;
         private Syncfusion.Windows.Forms.Tools.ToolStripEx toolStripEx1;
         private System.Windows.Forms.ToolStripButton ListaProductostoolStripButton1;
